a7379cff76a386914980346e536c2d6b8381003e
camilopr11 <camilop.rivas@gmail.com>
2020-11-19 14:55:26 -0500

    encyclopedias controller fix

--- src/controllers/enciclopedia_controller.js
+++ src/controllers/enciclopedia_controller.js
@@ -2,10 +2,10 @@ import { connect } from '../database/connection'
 
 class enciclopediaController {
 
-    static async createInfo(info) {
+    async createInfo(info) {
         try {
             const db = await connect()
-            const result = await db.collection('enciclopedia').insertOne(info)
+            const result = await db.collection('enciclopedias').insertOne(info)
             return result
         } catch (e) {
             return e
@@ -13,18 +13,18 @@ class enciclopediaController {
     }
     
 
-    static async listInfo(){
+    async listInfo(){
         const db = await connect()
-        const enciclopedia = await db.collection('enciclopedia').find().sort({date: 'desc'});
+        const enciclopedia = await db.collection('enciclopedias').find().sort({date: 'desc'});
         return enciclopedia;
     }
 
-    static async search(title){
+    async search(title){
         try{
             const db = await connect()
             const info = title.title;
             console.log('esta es info: ' +info);
-            const enciclopedia = await db.collection('enciclopedia').find({"title": {'$regex': info }});
+            const enciclopedia = await db.collection('enciclopedias').find({"title": {'$regex': info }});
             return enciclopedia;
         }catch(e){
             return e
